d9e6d2d12f05f6d9123a3aa63d96772d8147c080
OsmanW <osmanwako411@gmail.com>
2023-10-26 12:12:56 +0300

    Add Login Form using js

--- main.js
+++ main.js
@@ -11,9 +11,49 @@ function addcontent() {
     root.appendChild(header);
     root.appendChild(main);
 }
+
+function loginlabel(name) {
+    const label = document.createElement("label");
+    label.className = "login-control";
+    label.textContent = name;
+    return label;
+}
+
+function logininput(type) {
+    const input = document.createElement("input");
+    input.classList = "login-control input-login";
+    input.type = type;
+    return input;
+}
+
+function loginspanbtn() {
+    const span = document.createElement("span");
+    span.classList = "login-control login-span";
+    const btn = document.createElement("button");
+    btn.className = "button-login";
+    btn.textContent = "Login";
+    span.appendChild(btn);
+    return span;
+}
+
 function loginpage() {
     const temp = document.createElement("div");
-    temp.textContent = "I am login page";
+    temp.className = "login-page";
+    const title = document.createElement("h3");
+    title.textContent = "Welcome to Student Login Page";
+    temp.append(title);
+    //
+    const formlogin = document.createElement("form");
+    formlogin.className = "form-login";
+
+    formlogin.appendChild(loginlabel("Username or Email"));
+    formlogin.appendChild(logininput("text"));
+    formlogin.appendChild(loginlabel("Password"));
+    formlogin.appendChild(logininput("password"));
+    formlogin.appendChild(loginspanbtn());
+    temp.appendChild(formlogin);
+    //addloginform()
+    //
     main.innerHTML = "";
     main.append(temp);
 }
@@ -43,6 +83,5 @@ function createheader() {
     addregisterlink();
     addcontent();
 }
-
 
 createheader();
