c0dad4fdce79cadf0f8bf9d47ae8c03e93c9ba62
OsmanW <osmanwako411@gmail.com>
2023-10-27 09:22:29 +0300

    Add Login Module

--- login.js
+++ login.js
@@ -1,5 +1,43 @@
-export function Login() {
-    const container = document.createElement('div');
-    container.textContent = "logincontent"
-    return container;
+function loginlabel(name) {
+    const label = document.createElement("label");
+    label.className = "login-control";
+    label.textContent = name;
+    return label;
+}
+
+function logininput(type) {
+    const input = document.createElement("input");
+    input.classList = "login-control input-login";
+    input.type = type;
+    return input;
+}
+
+function loginspanbtn() {
+    const span = document.createElement("span");
+    span.classList = "login-control login-span";
+    const btn = document.createElement("button");
+    btn.className = "button-login";
+    btn.textContent = "Login";
+    span.appendChild(btn);
+    return span;
+}
+
+export function LoginPage() {
+    const login = document.createElement("div");
+    login.className = "login-page";
+
+    const title = document.createElement("h3");
+    title.textContent = "Welcome to Student Login Page";
+    login.append(title);
+
+    const formlogin = document.createElement("form");
+    formlogin.className = "form-login";
+
+    formlogin.appendChild(loginlabel("Username or Email"));
+    formlogin.appendChild(logininput("text"));
+    formlogin.appendChild(loginlabel("Password"));
+    formlogin.appendChild(logininput("password"));
+    formlogin.appendChild(loginspanbtn());
+    login.appendChild(formlogin);
+    return login;
 }
--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-
+import { LoginPage } from './login.js'
 const root = document.getElementById('root');
 const header = document.createElement('header');
 const nav = document.createElement('nav');
-const login = document.createElement('a');
+const loginlink = document.createElement('a');
 const register = document.createElement('a');
 const main = document.createElement('main');
 
@@ -12,56 +12,16 @@ function addcontent() {
     root.appendChild(main);
 }
 
-function loginlabel(name) {
-    const label = document.createElement("label");
-    label.className = "login-control";
-    label.textContent = name;
-    return label;
-}
-
-function logininput(type) {
-    const input = document.createElement("input");
-    input.classList = "login-control input-login";
-    input.type = type;
-    return input;
-}
-
-function loginspanbtn() {
-    const span = document.createElement("span");
-    span.classList = "login-control login-span";
-    const btn = document.createElement("button");
-    btn.className = "button-login";
-    btn.textContent = "Login";
-    span.appendChild(btn);
-    return span;
-}
-
 function loginpage() {
-    const temp = document.createElement("div");
-    temp.className = "login-page";
-    const title = document.createElement("h3");
-    title.textContent = "Welcome to Student Login Page";
-    temp.append(title);
-    //
-    const formlogin = document.createElement("form");
-    formlogin.className = "form-login";
-
-    formlogin.appendChild(loginlabel("Username or Email"));
-    formlogin.appendChild(logininput("text"));
-    formlogin.appendChild(loginlabel("Password"));
-    formlogin.appendChild(logininput("password"));
-    formlogin.appendChild(loginspanbtn());
-    temp.appendChild(formlogin);
-    //addloginform()
-    //
     main.innerHTML = "";
-    main.append(temp);
+    main.appendChild(LoginPage())
 }
+
 function addloginlink() {
-    login.textContent = "Login";
-    login.className = "menu-item";
-    login.addEventListener("click", loginpage);
-    nav.appendChild(login);
+    loginlink.textContent = "Login";
+    loginlink.className = "menu-item";
+    loginlink.addEventListener("click", loginpage);
+    nav.appendChild(loginlink);
 }
 
 function registerpage() {
